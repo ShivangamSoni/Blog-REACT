df53e8ea4ca7020e3dee5df89d3e599885aea2ef
Shivangam Soni <shivangamsoni99@gmail.com>
2021-11-13 21:59:45 +0530

    Write Page Basic Markup

--- src/Pages/Write/index.js
+++ src/Pages/Write/index.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import style from "./style.module.css";
+
 const Write = () => {
-  return <h1>Write Post</h1>;
+  const [textareaT, setT] = useState("");
+
+  const handleContentChange = (e) => {
+    const textarea = e.target;
+    setT(textarea.value);
+    console.log(textareaT);
+    console.log(JSON.stringify({ textareaT }));
+    // Auto Resize of Textarea
+    textarea.style.height = `auto`;
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
+  return (
+    <div>
+      <form className={style.form}>
+        <div className={style.formGroup}>
+          <input className={style.headingInput} type="text" placeholder="Title" autoFocus={true} />
+        </div>
+
+        <div className={style.formGroup}>
+          <i className={`fas fa-times ${style.deleteBtn}`}></i>
+
+          <textarea value={textareaT} onChange={handleContentChange} className={style.contentTextArea} placeholder="Paragraph"></textarea>
+        </div>
+
+        <div className={style.formGroup}>
+          <i className={`fas fa-times ${style.deleteBtn}`}></i>
+
+          <label>
+            <i className={`fas fa-plus ${style.imageBtn}`}></i>
+
+            <input className={style.imageInput} type="file" />
+          </label>
+        </div>
+
+        <button className={style.btn} type="button">
+          Add Section
+        </button>
+
+        <button className={`${style.submitBtn} ${style.btn}`} type="submit">
+          Post
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Write;
